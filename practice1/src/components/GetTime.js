5d36974a0e11b30c4712a3a7b2a10253cdf44ab1
Ramiz.sbai <ramizsbai@hotmail.com>
2019-08-07 20:33:02 +0200

    practicing inline style

--- practice1/src/components/GetTime.js
+++ practice1/src/components/GetTime.js
@@ -5,16 +5,22 @@ function GetTime () {
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay
+    const styles = {
+        fontSize: 40 
+    }
 
     if(hours < 12){
         timeOfDay = " Morning"
+        styles.color = "red"
     }else if (hours >= 12 && hours < 17){
         timeOfDay = " after noon"
+        styles.color = "blue"
     }else{
         timeOfDay = " night"
+        styles.color = "green"
     }
 
-    return(<h3>Good {timeOfDay} !</h3>)
+    return(<h3 style={styles}>Good {timeOfDay} !</h3>)
 
 }
 
